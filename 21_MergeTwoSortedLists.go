8e7986d9aef771f86156253d48e3a004801c1e1e
Pluto00 <1834733586@qq.com>
2019-12-24 14:47:02 +0800

    day 3

---
+++ 21_MergeTwoSortedLists.go
@@ -0,0 +1,32 @@
+package main
+
+/*
+将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/merge-two-sorted-lists
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	} else if l2 == nil {
+		return l1
+	}
+
+	if l1.Val < l2.Val {
+		l1.Next = mergeTwoLists(l1.Next, l2)
+		return l1
+	} else {
+		l2.Next = mergeTwoLists(l1, l2.Next)
+		return l2
+	}
+}
--- 9_PalindromeNumber.go
+++ 9_PalindromeNumber.go
@@ -1,7 +1,17 @@
 package main
 
 import "fmt"
+/*
+判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
 
+进阶:
+
+你能不将整数转为字符串来解决这个问题吗？
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/palindrome-number
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
 func isPalindrome(x int) bool {
 	if x < 0 {
 		return false
