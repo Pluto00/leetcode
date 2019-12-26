ee83003d6880a8fe48350daa05d0fb9dc26c0ba1
Pluto00 <1834733586@qq.com>
2019-12-26 19:56:17 +0800

    day 4

--- 28_ImplementStrstr.go
+++ 28_ImplementStrstr.go
@@ -1,19 +1,13 @@
 package main
 
-/*
-实现 strStr() 函数。
-
-给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。
-
-来源：力扣（LeetCode）
-链接：https://leetcode-cn.com/problems/implement-strstr
-著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
- */
-import "strings"
-
 func strStr(haystack string, needle string) int {
-	if needle == ""{
+	if needle == "" {
 		return 0
 	}
-	return strings.Index(haystack, needle)
+	for i := 0; i < len(haystack)-len(needle)+1; i++ {
+		if haystack[i:i+len(needle)] == needle {
+			return i
+		}
+	}
+	return -1
 }
