102decdc797a7de63fc793456c0ed095369cf233
Pluto00 <1834733586@qq.com>
2020-01-03 20:25:04 +0800

    day 12

---
+++ Algorithm/191_NumberOf1bits.go
@@ -0,0 +1,18 @@
+package main
+
+/*
+编写一个函数，输入是一个无符号整数，返回其二进制表达式中数字位数为 ‘1’ 的个数（也被称为汉明重量）。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/number-of-1-bits
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+func hammingWeight(num uint32) int {
+	var ans int
+	for num != 0 {
+		ans++
+		num &= num - 1
+	}
+	return ans
+}
