8ffabce6c4530fa85e3132c4c25afc793f4521af
Pluto00 <1834733586@qq.com>
2020-01-11 23:53:14 +0800

    day 20

---
+++ Algorithm/371_SumOfTwoIntegers.go
@@ -0,0 +1,17 @@
+package main
+
+/*
+不使用运算符 + 和 - ​​​​​​​，计算两整数 ​​​​​​​a 、b ​​​​​​​之和。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/sum-of-two-integers
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+func getSum(a int, b int) int {
+	for b != 0 {
+		a = a ^ b        // 无进位加法
+		b = (a & b) << 1 // 获取进位
+	}
+	return a
+}
