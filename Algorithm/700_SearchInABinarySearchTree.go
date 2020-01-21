399edf45df96b00a0efbcc919f78f9ee306d7204
Pluto00 <1834733586@qq.com>
2020-01-21 13:28:28 +0800

    day 30

---
+++ Algorithm/700_SearchInABinarySearchTree.go
@@ -0,0 +1,24 @@
+package main
+
+import "encoding/asn1"
+
+/*
+给定二叉搜索树（BST）的根节点和一个值。 你需要在BST中找到节点值等于给定值的节点。 返回以该节点为根的子树。 如果节点不存在，则返回 NULL。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/search-in-a-binary-search-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+func searchBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Val == val {
+		return root
+	} else if root.Val < val {
+		return searchBST(root.Left, val)
+	} else {
+		return searchBST(root.Right, val)
+	}
+}
