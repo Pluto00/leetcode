c192228dd80615ed4eebecdafd188c38c420a028
Pluto00 <1834733586@qq.com>
2020-01-30 12:43:47 +0800

    day 39

---
+++ Algorithm/669_TrimABinarySearchTree.go
@@ -0,0 +1,32 @@
+package main
+
+/*
+给定一个二叉搜索树，同时给定最小边界L 和最大边界 R。通过修剪二叉搜索树，使得所有节点的值在[L, R]中 (R>=L) 。你可能需要改变树的根节点，所以结果应当返回修剪好的二叉搜索树的新的根节点。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/trim-a-binary-search-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func trimBST(root *TreeNode, L int, R int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Val < L {
+		return trimBST(root.Right, L, R)
+	}
+	if root.Val > R {
+		return trimBST(root.Left, L, R)
+	}
+	root.Left = trimBST(root.Left, L, R)
+	root.Right = trimBST(root.Right, L, R)
+	return root
+}
