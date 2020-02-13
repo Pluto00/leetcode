e684b6d3deb8b226612c37abcc2786a9ccdc94d0
Pluto00 <1834733586@qq.com>
2020-02-13 22:55:39 +0800

    day 53

---
+++ Algorithm/63_UniquePathsII.go
@@ -0,0 +1,67 @@
+package main
+
+/*
+一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+
+机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+
+现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
+
+网格中的障碍物和空位置分别用 1 和 0 来表示。
+
+说明：m 和 n 的值均不超过 100。
+
+示例 1:
+
+输入:
+[
+  [0,0,0],
+  [0,1,0],
+  [0,0,0]
+]
+输出: 2
+解释:
+3x3 网格的正中间有一个障碍物。
+从左上角到右下角一共有 2 条不同的路径：
+1. 向右 -> 向右 -> 向下 -> 向下
+2. 向下 -> 向下 -> 向右 -> 向右
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/unique-paths-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+//动态规划，就地修改
+func uniquePathsWithObstacles(obstacleGrid [][]int) int {
+	var (
+		n = len(obstacleGrid)
+		m = len(obstacleGrid[0])
+	)
+	if obstacleGrid[0][0] == 1 || obstacleGrid[n-1][m-1] == 1 {
+		return 0
+	}
+
+	for i, flag := 0, 1; i < n; i++ {
+		if obstacleGrid[i][0] == 1 {
+			flag = 0
+		}
+		obstacleGrid[i][0] = flag
+	}
+	for i, flag := 1, 1; i < m; i++ {
+		if obstacleGrid[0][i] == 1 {
+			flag = 0
+		}
+		obstacleGrid[0][i] = flag
+	}
+
+	for i := 1; i < n; i++ {
+		for j := 1; j < m; j++ {
+			if obstacleGrid[i][j] == 1 {
+				obstacleGrid[i][j] = 0
+			} else {
+				obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1]
+			}
+		}
+	}
+	return obstacleGrid[n-1][m-1]
+}
