75e73699de6ed1fc2647d7e4df2ccef395b324fc
Pluto00 <1834733586@qq.com>
2020-02-20 22:06:52 +0800

    day 60

---
+++ Algorithm/98_ValidateBinarySearchTree.go
@@ -0,0 +1,66 @@
+package main
+
+/*
+给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+
+假设一个二叉搜索树具有如下特征：
+
+节点的左子树只包含小于当前节点的数。
+节点的右子树只包含大于当前节点的数。
+所有左子树和右子树自身必须也是二叉搜索树。
+示例 1:
+
+输入:
+    2
+   / \
+  1   3
+输出: true
+示例 2:
+
+输入:
+    5
+   / \
+  1   4
+     / \
+    3   6
+输出: false
+解释: 输入为: [5,1,4,null,null,3,6]。
+     根节点的值为 5 ，但是其右子节点值为 4 。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/validate-binary-search-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isValidBST(root *TreeNode) bool {
+	var (
+		inOrder func(*TreeNode)
+		ans     = true
+		first   = true
+		last    = -0x3f3f3f3f
+	)
+	inOrder = func(node *TreeNode) {
+		if node == nil || !ans {
+			return
+		}
+		inOrder(node.Left)
+		if !first && node.Val <= last {
+			ans = false
+			return
+		} else {
+			last = node.Val
+			first = false
+		}
+		inOrder(node.Right)
+	}
+	inOrder(root)
+	return ans
+}
