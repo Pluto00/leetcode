0a11b10f9aefbdd296de401dbf040ade916aa8b1
Pluto00 <1834733586@qq.com>
2020-02-22 23:16:00 +0800

    day 62

---
+++ Algorithm/5323_SortIntersByTheNumberOf1Bits.go
@@ -0,0 +1,45 @@
+package main
+
+import "sort"
+
+/*
+给你一个整数数组 arr 。请你将数组中的元素按照其二进制表示中数字 1 的数目升序排序。
+
+如果存在多个数字二进制中 1 的数目相同，则必须将它们按照数值大小升序排列。
+
+请你返回排序后的数组。
+
+
+
+示例 1：
+
+输入：arr = [0,1,2,3,4,5,6,7,8]
+输出：[0,1,2,4,8,3,5,6,7]
+解释：[0] 是唯一一个有 0 个 1 的数。
+[1,2,4,8] 都有 1 个 1 。
+[3,5,6] 有 2 个 1 。
+[7] 有 3 个 1 。
+按照 1 的个数排序得到的结果数组为 [0,1,2,4,8,3,5,6,7]
+*/
+
+func sortByBits(arr []int) []int {
+	countOf1bits := func(num int) int {
+		cnt := 0
+		for num > 0 {
+			if num&1 == 1 {
+				cnt++
+			}
+			num >>= 1
+		}
+		return cnt
+	}
+	sort.Slice(arr, func(i, j int) bool {
+		countI := countOf1bits(arr[i])
+		countJ := countOf1bits(arr[j])
+		if countI == countJ {
+			return arr[i] < arr[j]
+		}
+		return countI < countJ
+	})
+	return arr
+}
