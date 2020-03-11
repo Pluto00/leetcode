9438c9329ab9a5eeeb49a3357836fe9b3535e998
Pluto00 <1834733586@qq.com>
2020-03-11 22:14:47 +0800

    day 76

---
+++ Algorithm/424_LongestRepeatingCharacterReplacement.go
@@ -0,0 +1,62 @@
+package main
+
+/*
+给你一个仅由大写英文字母组成的字符串，你可以将任意位置上的字符替换成另外的字符，总共可最多替换 k 次。在执行上述操作后，找到包含重复字母的最长子串的长度。
+
+注意:
+字符串长度 和 k 不会超过 104。
+
+示例 1:
+
+输入:
+s = "ABAB", k = 2
+
+输出:
+4
+
+解释:
+用两个'A'替换为两个'B',反之亦然。
+示例 2:
+
+输入:
+s = "AABABBA", k = 1
+
+输出:
+4
+
+解释:
+将中间的一个'A'替换为'B',字符串变为 "AABBBBA"。
+子串 "BBBB" 有最长重复字母, 答案为 4。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/longest-repeating-character-replacement
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+func characterReplacement(s string, k int) int {
+	if len(s) <= 0 {
+		return 0
+	}
+	var (
+		chMap   [26]int
+		left    int
+		right   int
+		charMax int
+		max     = func(a, b int) int {
+			if a > b {
+				return a
+			} else {
+				return b
+			}
+		}
+	)
+	for ; right < len(s); right++ {
+		chMap[s[right]-'A']++
+		charMax = max(charMax, chMap[s[right]-'A'])
+		if right-left+1 > charMax+k {
+			chMap[s[left]-'A']--
+			left++
+		}
+	}
+	return len(s) - left
+}
