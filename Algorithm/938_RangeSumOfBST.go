bef3cc35b5ae8fce45f1b36b7dc3d77efa387d23
Pluto00 <1834733586@qq.com>
2020-03-17 19:35:36 +0800

    day 81

---
+++ Algorithm/938_RangeSumOfBST.go
@@ -0,0 +1,58 @@
+package main
+
+/*
+给定二叉搜索树的根结点 root，返回 L 和 R（含）之间的所有结点的值的和。
+
+二叉搜索树保证具有唯一的值。
+
+
+
+示例 1：
+
+输入：root = [10,5,15,3,7,null,18], L = 7, R = 15
+输出：32
+示例 2：
+
+输入：root = [10,5,15,3,7,13,18,1,null,6], L = 6, R = 10
+输出：23
+
+
+提示：
+
+树中的结点数量最多为 10000 个。
+最终的答案保证小于 2^31。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/range-sum-of-bst
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func rangeSumBST(root *TreeNode, L int, R int) int {
+	var (
+		dfs func(*TreeNode)
+		sum = 0
+	)
+	dfs = func(node *TreeNode) {
+		if node != nil {
+			if L <= node.Val && node.Val <= R {
+				sum += node.Val
+			}
+			if node.Val > L {
+				dfs(node.Left)
+			}
+			if node.Val < R {
+				dfs(node.Right)
+			}
+		}
+	}
+	dfs(root)
+	return sum
+}
