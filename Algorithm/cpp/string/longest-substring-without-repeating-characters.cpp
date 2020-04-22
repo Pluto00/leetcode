34cf1e9f1e05a426da38c1ade778a5a968595102
Pluto00 <1834733586@qq.com>
2020-04-22 23:33:48 +0800

    day 113

--- Algorithm/cpp/string/longest-substring-without-repeating-characters.cpp
+++ Algorithm/cpp/string/longest-substring-without-repeating-characters.cpp
@@ -31,7 +31,7 @@ public:
         vector<int> idx(128);
         int ans = 0;
         for (int i = 0, j = 0; j < s.size(); j++)
-        {
+        {`
             i = max(idx[s[j]], i);
             ans = max(ans, j - i + 1);
             idx[s[j]] = j + 1;
