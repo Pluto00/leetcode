e5ce1211278c5df8ba2a718e216a5d38bccb81b0
Pluto00 <1834733586@qq.com>
2020-07-10 22:57:55 +0800

    day 186

---
+++ Algorithm/cpp/math/majority-element.cpp
@@ -0,0 +1,36 @@
+/*
+给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+
+你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+
+ 
+
+示例 1:
+
+输入: [3,2,3]
+输出: 3
+示例 2:
+
+输入: [2,2,1,1,1,2,2]
+输出: 2
+通过次数184,930提交次数289,924
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/majority-element
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+class Solution
+{
+public:
+    int majorityElement(vector<int> &nums)
+    {
+        int ans = nums[0], cnt = 1;
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (cnt == 0)
+                ans = nums[i];
+            nums[i] == ans ? cnt++ : cnt--;
+        }
+        return ans;
+    }
+};
