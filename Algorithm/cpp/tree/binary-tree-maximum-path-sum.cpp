1753e00a5ef83803f20de6d15562093b349f69c3
Pluto00 <1834733586@qq.com>
2020-07-21 19:11:06 +0800

    day 191

---
+++ Algorithm/cpp/tree/binary-tree-maximum-path-sum.cpp
@@ -0,0 +1,59 @@
+/*
+给定一个非空二叉树，返回其最大路径和。
+
+本题中，路径被定义为一条从树中任意节点出发，达到任意节点的序列。该路径至少包含一个节点，且不一定经过根节点。
+
+示例 1:
+
+输入: [1,2,3]
+
+       1
+      / \
+     2   3
+
+输出: 6
+示例 2:
+
+输入: [-10,9,20,null,null,15,7]
+
+   -10
+   / \
+  9  20
+    /  \
+   15   7
+
+输出: 42
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/binary-tree-maximum-path-sum
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution
+{
+    int ans = INT_MIN;
+    int depth(TreeNode *root)
+    {
+        if (root == nullptr)
+            return 0;
+        int L = max(depth(root->left), 0);
+        int R = max(depth(root->right), 0);
+        ans = max(ans, L + R + root->val);
+        return root->val + max(L, R);
+    }
+
+public:
+    int maxPathSum(TreeNode *root)
+    {
+        depth(root);
+        return ans;
+    }
+};
